05a1897b14810a2073407339b8ca57457c8722c3
uGooone <yann.grosleziat@gmail.com>
2021-02-22 21:49:12 +0100

    Update index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+ <html>
+
+        <head>
+                <meta charset="utf-8" />                        <!-- Police--> 
+                <title>PARKING Newton</title>                   <!--Titre"Parking Newton (au niveau de l'onglet) --> 
+                <link rel="stylesheet" href="style2.css"/>      <!-- déclare le css--> 
+        </head>
+        </html>
